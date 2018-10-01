a9f95882b9f7a75aa381e9b059a6d58d564ea91c
Stelios Sotiriadis <steliosot@msn.com>
2018-10-01 21:42:50 +0100

    Update..

---
+++ plot1.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+plt.plot([1,4,9,16,25,36], color='green', marker='o', linestyle='solid')
+plt.title("Visualization of my list")
+plt.xlabel('position')
+plt.ylabel('value')
+plt.show()
+
