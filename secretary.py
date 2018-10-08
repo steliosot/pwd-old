d7bf2f1d52e9d26c0d377ac7b086974089745b2b
steliosot <steliosot@msn.com>
2018-10-08 14:36:40 +0100

    secretary

---
+++ secretary.py
@@ -0,0 +1,20 @@
+#Solving the probabilities for the Secretary problem
+import math
+import matplotlib.pyplot as plt
+
+cand_list = range(3,16)
+print([i for i in cand_list])
+
+optimal_sample_size_k = [int(round(i/2.71,0)) for i in cand_list]
+print([i for i in optimal_sample_size_k])
+
+ratio_k_n = [round(optimal_sample_size_k[i]/cand_list[i],4) for i in range(len(optimal_sample_size_k))]
+print(ratio_k_n)
+
+approx_prob_success_p = [round((i*math.log(i)*-1),4) for i in ratio_k_n]
+#(ratio_k_n[i]*math.log(ratio_k_n[i],2.71))
+print(approx_prob_success_p)
+
+fig, ax = plt.subplots()
+
+ax.plot(approx_prob_success_p)
